542534dc0248e8bc85156992062c1ec35a25f8e8
Akshat Singla <77603490+akshat-singla23@users.noreply.github.com>
2024-03-04 17:12:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if ingridents_list:
     st.write(ingridents_string)
     import requests
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response.json())
+    #st.text(fruityvice_response.json())
+    fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order, order_filled)
             values ('""" + ingridents_string + """','"""+name_on_order+ """','"""+'FALSE'+ """')"""
     st.write(my_insert_stmt)
